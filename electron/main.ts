c5538f8fb6935d8fe71d021a7008195cb66ea784
Tales Garcia <garcia.tales@gmail.com>
2021-03-10 15:24:56 -0300

    feat(main): display frameless window

--- electron/main.ts
+++ electron/main.ts
@@ -2,16 +2,27 @@ import { app, BrowserWindow } from 'electron'
 import * as path from 'path'
 import * as url from 'url'
 
-let mainWindow: Electron.BrowserWindow | null
+let mainWindow: Electron.BrowserWindow | null;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 1100,
-    height: 700,
+    width: 400,
+    height: 500,
     backgroundColor: '#191622',
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+      enableRemoteModule: true
+    },
+    movable: true,
+    resizable: true,
+    maximizable: false,
+    minimizable: false,
+    titleBarStyle: 'hiddenInset',
+    alwaysOnTop: true,
+    fullscreenable: false,
+    frame: process.platform === 'darwin',
+    transparent: process.platform === 'darwin',
+    acceptFirstMouse: true
   })
 
   if (process.env.NODE_ENV === 'development') {
