6b61a364786cdffadb73148b8f236b4f4c2fe5d4
Tales Garcia <garcia.tales@gmail.com>
2021-03-10 15:26:38 -0300

    feat(app.tsx): create draggable span

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { GlobalStyle } from './styles/GlobalStyle'
 
 const mainElement = document.createElement('div')
 mainElement.setAttribute('id', 'root')
+const windowTopBar = document.createElement('span')
+windowTopBar.setAttribute('style', 'width: 100%; height: 36px; -webkit-app-region: drag; display: block;')
+document.body.appendChild(windowTopBar)
 document.body.appendChild(mainElement)
 
 const App = () => {
