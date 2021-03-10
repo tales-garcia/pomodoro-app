8db87ae9522787e415cf9c61591620e984864461
Tales Garcia <garcia.tales@gmail.com>
2021-03-10 13:17:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { GlobalStyle } from './styles/GlobalStyle'
+
+const mainElement = document.createElement('div')
+mainElement.setAttribute('id', 'root')
+document.body.appendChild(mainElement)
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <div>Hello</div>
+    </>
+  )
+}
+
+render(<App />, mainElement)
