b0135a311e84821a985fbd3965766b861d78698a
Tales Garcia <garcia.tales@gmail.com>
2021-03-11 14:28:12 -0300

    feat(app): set theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import 'react-circular-progressbar/dist/styles.css';
 import { render } from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+import Clock from './components/Clock'
 import { GlobalStyle } from './styles/GlobalStyle'
 
 const mainElement = document.createElement('div')
@@ -9,12 +12,19 @@ windowTopBar.setAttribute('style', 'width: 100%; height: 36px; -webkit-app-regio
 document.body.appendChild(windowTopBar)
 document.body.appendChild(mainElement)
 
+const theme = {
+  blue: '#1A1A29',
+  darkBlue: '#151423',
+  red: '#D93C3C',
+  text: '#E4E4E4'
+}
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>Hello</div>
-    </>
+      <Clock />
+    </ThemeProvider>
   )
 }
 
