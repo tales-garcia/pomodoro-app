f07c37327c0318e2335a4bd86d94d7cdd9dc4c32
Tales Garcia <garcia.tales@gmail.com>
2021-06-21 12:06:24 -0300

    feat(timer item): add event to delete button

--- src/components/TimerItem/index.tsx
+++ src/components/TimerItem/index.tsx
@@ -2,6 +2,7 @@ import { ipcRenderer } from 'electron';
 import React, { useCallback, useMemo } from 'react';
 import { FiPlay, FiEdit, FiTrash } from 'react-icons/fi';
 import { useTheme } from 'styled-components';
+import { useWorkspace } from '../../hooks/workspace';
 
 import { Container } from './styles';
 
@@ -11,6 +12,7 @@ interface TimerItemProps {
 
 const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
     const { red, text } = useTheme();
+    const { deleteTimer } = useWorkspace();
 
     const minutes = useMemo(() => time ? Math.floor(time / 60) : null, [time]);
     const seconds = useMemo(() => time ? time % 60 : null, [time]);
@@ -32,7 +34,7 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
             <footer>
                 <FiPlay onClick={openTimer} size={20} color={text} />
                 <FiEdit size={20} color={text} />
-                <FiTrash size={20} color={red} />
+                <FiTrash size={20} color={red} onClick={() => deleteTimer(id)} />
             </footer>
         </Container>
     );
