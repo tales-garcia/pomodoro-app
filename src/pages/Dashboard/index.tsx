f7444d19b1fd1e64a4566131923278240dfffe4c
Tales Garcia <garcia.tales@gmail.com>
2021-06-21 12:03:53 -0300

    refactor(dashboard): use data from hook

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,140 +1,13 @@
-import { ipcRenderer } from 'electron';
-import { Form, Formik } from 'formik';
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { FiTrash2, FiPlus, FiFolderPlus, FiSliders } from 'react-icons/fi';
 import { useTheme } from 'styled-components';
-import Input from '../../components/Input';
 import TimerItem from '../../components/TimerItem';
-import { useModal } from '../../hooks/modal';
-import { Container, WorkspaceItem, TimeInput, ButtonsContainer } from './styles';
-import * as yup from 'yup';
-
-const workspaceValidation = yup.object().shape({
-  name: yup.string().required('Name is required')
-});
-
-const timerValidation = yup.object().shape({
-  name: yup.string().required('Name is required'),
-  hours: yup.number(),
-  minutes: yup.number(),
-  seconds: yup.number().required('Seconds are required')
-});
+import { Container, WorkspaceItem } from './styles';
+import { useWorkspace } from '../../hooks/workspace';
 
 const Dashboard: React.FC = () => {
   const { red, text } = useTheme();
-  const [workspaces, setWorkspaces] = useState<Workspace[]>(ipcRenderer.sendSync('get-workspaces'));
-  const [selectedWorkspace, setSelectedWorkspace] = useState<Workspace | null>(null);
-  const { setContent, show, hide } = useModal();
-
-  const createWorkspace = useCallback(({ name }) => {
-    hide();
-
-    const workspace = {
-      name: name,
-      timers: []
-    }
-
-    const generatedWorkspace = ipcRenderer.sendSync('create-workspace', workspace) as Workspace;
-
-    if (generatedWorkspace) {
-      setWorkspaces((prevState) => [...prevState, generatedWorkspace]);
-    }
-  }, []);
-
-  const deleteTimer = useCallback((id: string) => {
-    setWorkspaces(ipcRenderer.sendSync('delete-timer', id))
-  }, []);
-
-  const handleWorkspaceDelete = (id: string) => {
-    if (!selectedWorkspace || id !== selectedWorkspace.id) {
-      setWorkspaces(ipcRenderer.sendSync('delete-workspace', id));
-      return;
-    }
-
-    let selectedWorkspaceIndex = workspaces.findIndex((workspace) => workspace.id === selectedWorkspace.id);
-
-    if (workspaces[selectedWorkspaceIndex + 1]) selectedWorkspaceIndex++;
-    else if (workspaces[selectedWorkspaceIndex - 1]) selectedWorkspaceIndex--;
-    else {
-      setSelectedWorkspace(null);
-      setWorkspaces(ipcRenderer.sendSync('delete-workspace', id));
-      return;
-    }
-    setSelectedWorkspace(workspaces[selectedWorkspaceIndex]);
-    setWorkspaces(ipcRenderer.sendSync('delete-workspace', id));
-  }
-
-  const createWorkspaceModal = useCallback(() => {
-    setContent(
-      (
-        <Formik
-          initialValues={{ name: '' }}
-          validationSchema={workspaceValidation}
-          validateOnBlur={false}
-          validateOnChange={false}
-          onSubmit={createWorkspace}
-        >
-          <Form>
-            <h2>New workspace</h2>
-            <Input name="name" type="text" placeholder="Name" />
-            <ButtonsContainer>
-              <button type="button" onClick={hide}>Cancel</button>
-              <button type="submit">Confirm</button>
-            </ButtonsContainer>
-          </Form>
-        </Formik>
-      )
-    );
-
-    show();
-  }, []);
-
-  const createTimer = useCallback(({ name, hours, minutes, seconds, workspaceId }) => {
-    hide();
-
-    const finalTimer: ITimerDTO = {
-      name,
-      time: (hours * 60 * 60) + (minutes * 60) + seconds,
-      workspaceId
-    }
-
-    ipcRenderer.sendSync('create-timer', finalTimer);
-
-    setSelectedWorkspace((ipcRenderer.sendSync('get-workspaces') as Workspace[]).find(workspace => workspace.id === workspaceId) || null);
-    setWorkspaces(ipcRenderer.sendSync('get-workspaces'));
-  }, []);
-
-  const createNewTimerModal = useCallback(() => {
-    if (!selectedWorkspace) return;
-
-    setContent(
-      (
-        <Formik
-          initialValues={{ name: '', hours: '', minutes: '', seconds: '', workspaceId: selectedWorkspace.id }}
-          onSubmit={createTimer}
-          validateOnBlur={false}
-          validateOnChange={false}
-          validationSchema={timerValidation}
-        >
-          <Form>
-            <h2>New timer</h2>
-            <Input name="name" type="text" placeholder="Name" />
-            <TimeInput>
-              <Input name="hours" type="number" placeholder="Hours" />:
-              <Input name="minutes" type="number" placeholder="Minutes" />:
-              <Input name="seconds" type="number" placeholder="Seconds" />
-            </TimeInput>
-            <ButtonsContainer>
-              <button type="button" onClick={hide}>Cancel</button>
-              <button type="submit">Confirm</button>
-            </ButtonsContainer>
-          </Form>
-        </Formik>
-      )
-    );
-
-    show();
-  }, [selectedWorkspace]);
+  const { createNewTimerModal, workspaces, selectedWorkspace, createWorkspaceModal, setSelectedWorkspace, deleteWorkspace } = useWorkspace();
 
   return (
     <Container>
@@ -146,7 +19,7 @@ const Dashboard: React.FC = () => {
               {workspace.name}
               <FiTrash2 size={16} color={red} onClick={e => {
                 e.stopPropagation();
-                handleWorkspaceDelete(workspace.id);
+                deleteWorkspace(workspace.id);
               }} />
             </WorkspaceItem>
           ))}
