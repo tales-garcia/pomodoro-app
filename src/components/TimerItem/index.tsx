d6d897f9d48da38e171c95504b993a5a1e4c95b1
Tales Garcia <garcia.tales@gmail.com>
2021-07-02 13:50:15 -0300

    feat(timer item): animate everything

--- src/components/TimerItem/index.tsx
+++ src/components/TimerItem/index.tsx
@@ -4,6 +4,7 @@ import { FiPlay, FiEdit, FiTrash } from 'react-icons/fi';
 import { useTheme } from 'styled-components';
 import { useWorkspace } from '../../hooks/workspace';
 import Button from '../Button';
+import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
 
 import { Container, ButtonsContainer } from './styles';
 
@@ -16,6 +17,7 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
     const { deleteTimer } = useWorkspace();
 
     const [editMode, setEditMode] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
     const hours = useMemo(() => time ? Math.floor(time / 3600) : null, [time]);
     const minutes = useMemo(() => time ? Math.floor(time % 3600 / 60) : null, [time]);
@@ -37,22 +39,39 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
     const toggleEditMode = useCallback(() => setEditMode(prev => !prev), []);
 
     return (
-        <Container>
-            <h2>{!!hours && <>{stringHours}<span>:</span></>}{stringMinutes}<span>:</span>{stringSeconds}</h2>
-            <h3>{name}</h3>
-            {!editMode && (
-                <footer>
-                    <FiPlay onClick={openTimer} size={20} color={text} />
-                    <FiEdit size={20} color={text} onClick={toggleEditMode} />
-                    <FiTrash size={20} color={red} onClick={handleDeleteTimer} />
-                </footer>
-            )}
-            {editMode && (
-                <ButtonsContainer>
-                    <Button onClick={toggleEditMode}>Cancel</Button>
-                    <Button confirmButton>Confirm</Button>
-                </ButtonsContainer>
-            )}
+        <Container
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <AnimateSharedLayout>
+                <motion.h2 layoutId="time">{!!hours && <>{stringHours}<span>:</span></>}{stringMinutes}<span>:</span>{stringSeconds}</motion.h2>
+                <motion.h3 layoutId="name">{name}</motion.h3>
+                <AnimatePresence>
+                    {!editMode && (
+                        <motion.footer
+                            animate={{ opacity: isHovered ? 1 : 0 }}
+                            initial={{ opacity: 0 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <FiPlay onClick={openTimer} size={20} color={text} />
+                            <FiEdit size={20} color={text} onClick={toggleEditMode} />
+                            <FiTrash size={20} color={red} onClick={handleDeleteTimer} />
+                        </motion.footer>
+                    )}
+                </AnimatePresence>
+                <AnimatePresence>
+                    {editMode && (
+                        <ButtonsContainer
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                            initial={{ opacity: 0 }}
+                        >
+                            <Button onClick={toggleEditMode}>Cancel</Button>
+                            <Button confirmButton>Confirm</Button>
+                        </ButtonsContainer>
+                    )}
+                </AnimatePresence>
+            </AnimateSharedLayout>
         </Container>
     );
 }
