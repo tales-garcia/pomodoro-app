08bd548dfd509c1e5e7a2150beb78f1ea1b1e568
Tales Garcia <garcia.tales@gmail.com>
2021-07-05 11:32:44 -0300

    feat(timer item): make content editable

--- src/components/TimerItem/index.tsx
+++ src/components/TimerItem/index.tsx
@@ -5,7 +5,7 @@ import { useTheme } from 'styled-components';
 import { useWorkspace } from '../../hooks/workspace';
 import Button from '../Button';
 import { AnimatePresence, AnimateSharedLayout, motion } from 'framer-motion';
-
+import NumberEditableContent from '../NumberEditableContent';
 import { Container, ButtonsContainer } from './styles';
 
 interface TimerItemProps {
@@ -14,7 +14,7 @@ interface TimerItemProps {
 
 const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
     const { red, text } = useTheme();
-    const { deleteTimer } = useWorkspace();
+    const { deleteTimer, editTimer } = useWorkspace();
 
     const [editMode, setEditMode] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
@@ -27,6 +27,9 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
     const stringMinutes = useMemo(() => String(minutes).padStart(2, '0'), [minutes]);
     const stringSeconds = useMemo(() => String(seconds).padStart(2, '0'), [seconds]);
 
+    const [showingValues, setShowingValues] = useState<{ [key: string]: any }>({ hours: stringHours, minutes: stringMinutes, seconds: stringSeconds, name });
+    const [editValues, setEditValues] = useState<{ [key: string]: any }>({ hours: stringHours, minutes: stringMinutes, seconds: stringSeconds, name });
+
     const openTimer = useCallback(() => {
         ipcRenderer.send('create-timer-window', {
             time,
@@ -36,7 +39,24 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
 
     const handleDeleteTimer = useCallback(() => deleteTimer(id), [id]);
 
-    const toggleEditMode = useCallback(() => setEditMode(prev => !prev), []);
+    const toggleEditMode = useCallback(() => {
+        setEditMode(prev => !prev)
+    }, []);
+
+    const handleEditTimer = useCallback(() => {
+        const { hours, minutes, seconds, name: editedName } = editValues;
+
+        const finalTime = (Number(hours) * 60 * 60) + (Number(minutes) * 60) + Number(seconds);
+
+        const finalValues: Partial<Timer> = {
+            time: finalTime,
+            name: editedName
+        };
+
+        setShowingValues(editValues);
+        setEditMode(false);
+        editTimer(id, finalValues);
+    }, [showingValues, id, editValues]);
 
     return (
         <Container
@@ -44,8 +64,55 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
             onMouseLeave={() => setIsHovered(false)}
         >
             <AnimateSharedLayout>
-                <motion.h2 layoutId="time">{!!hours && <>{stringHours}<span>:</span></>}{stringMinutes}<span>:</span>{stringSeconds}</motion.h2>
-                <motion.h3 layoutId="name">{name}</motion.h3>
+                {editMode ? (
+                    <motion.h2 layoutId="time">
+                        {!!hours && (
+                            <>
+                                <NumberEditableContent
+                                    onChange={value => setEditValues({ ...editValues, hours: value })}
+                                >
+                                        {showingValues.hours}
+                                </NumberEditableContent>
+                                <span>:</span>
+                            </>
+                        )}
+                        <NumberEditableContent
+                            onChange={value => setEditValues({ ...editValues, minutes: value })}
+                        >
+                            {showingValues.minutes}
+                        </NumberEditableContent>
+                        <span>:</span>
+                        <NumberEditableContent
+                            onChange={value => setEditValues({ ...editValues, seconds: value })}
+                        >
+                            {showingValues.seconds}
+                        </NumberEditableContent>
+                    </motion.h2>
+                ) : (
+                    <motion.h2 layoutId="time">{!!hours && <>{showingValues.hours}<span>:</span></>}{showingValues.minutes}<span>:</span>{showingValues.seconds}</motion.h2>
+                )}
+                <motion.h3
+                    contentEditable={editMode}
+                    layoutId="name"
+                    onKeyDown={e => {
+                        switch (e.key) {
+                            case 'Enter': {
+                                e.preventDefault();
+                                e.currentTarget.blur();
+                                break;
+                            }
+                            case 'Escape': {
+                                e.preventDefault();
+                                e.currentTarget.textContent = showingValues.name;
+                                e.currentTarget.blur();
+                                break;
+                            }
+                        }
+                    }}
+                    onInput={e => setEditValues({ ...editValues, name: e.currentTarget.textContent })}
+                >
+                    {showingValues.name}
+                </motion.h3>
                 <AnimatePresence>
                     {!editMode && (
                         <motion.footer
@@ -66,8 +133,15 @@ const TimerItem: React.FC<TimerItemProps> = ({ data: { name, time, id } }) => {
                             exit={{ opacity: 0 }}
                             initial={{ opacity: 0 }}
                         >
-                            <Button onClick={toggleEditMode}>Cancel</Button>
-                            <Button confirmButton>Confirm</Button>
+                            <Button 
+                                onClick={() => {
+                                    toggleEditMode();
+                                    setEditValues(showingValues);
+                                }}
+                            >
+                                Cancel
+                            </Button>
+                            <Button confirmButton onClick={handleEditTimer}>Confirm</Button>
                         </ButtonsContainer>
                     )}
                 </AnimatePresence>
