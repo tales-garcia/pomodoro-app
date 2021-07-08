c6367e0b388e11936cdcfbe7c2df12af171e209a
Tales Garcia <garcia.tales@gmail.com>
2021-07-08 19:35:22 -0300

    feat(timer): save timer to recents

--- src/pages/Timer/index.tsx
+++ src/pages/Timer/index.tsx
@@ -2,14 +2,29 @@ import { ipcRenderer, remote } from 'electron';
 import React, { useEffect } from 'react';
 import Clock from '../../components/Clock';
 import { Title } from './styles';
+import { v4 } from 'uuid';
 
 const {
-    storedTime, storedMaxTime, name
+    storedTime, storedMaxTime, name, id
 } = ipcRenderer.sendSync('get-timer-props');
 
 const Timer: React.FC = () => {
 
-    useEffect(() => remote.getCurrentWindow().setTitle(`${!!name ? `${name} - ` : 'Unkown - '}Timer`), []);
+    useEffect(() => {
+        remote.getCurrentWindow().setTitle(`${!!name ? `${name} - ` : 'Unkown - '}Timer`);
+
+        if (!!id) {
+            ipcRenderer.send('save-recent', id);
+        } else if (!!storedMaxTime) {
+            const finalTimer: Timer = {
+                id: v4(),
+                name: !!name ? name : 'Untitled',
+                time: storedMaxTime
+            };
+
+            ipcRenderer.send('save-recent', finalTimer);
+        }
+    }, []);
 
     return (
         <>
