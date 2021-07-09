e2a3a7e1d668f66e6c7eee638a29ff5149f6a393
Tales Garcia <garcia.tales@gmail.com>
2021-07-09 15:59:00 -0300

    feat(react.webpack): add support to images

--- webpack/react.webpack.js
+++ webpack/react.webpack.js
@@ -23,6 +23,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: {
+          loader: 'file-loader',
+        }
       }
     ]
   },
