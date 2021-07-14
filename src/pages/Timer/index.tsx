fbb3005ac45d2e75a1b813900a3c600c679f9303
Tales Garcia <garcia.tales@gmail.com>
2021-07-14 19:11:29 -0300

    refactor(timer page): clean file

--- src/pages/Timer/index.tsx
+++ src/pages/Timer/index.tsx
@@ -1,35 +1,15 @@
-import { ipcRenderer, remote } from 'electron';
-import React, { useEffect } from 'react';
+import React from 'react';
 import Clock from '../../components/Clock';
+import { useTimer } from '../../hooks/timer';
 import { Title } from './styles';
-import { v4 } from 'uuid';
-
-const {
-    storedTime, storedMaxTime, name, id
-} = ipcRenderer.sendSync('get-timer-props', remote.getCurrentWindow().id);
 
 const Timer: React.FC = () => {
-
-    useEffect(() => {
-        remote.getCurrentWindow().setTitle(`${!!name ? `${name} - ` : 'Untitled - '}Timer`);
-
-        if (!!id) {
-            ipcRenderer.send('save-recent', id);
-        } else if (!!storedMaxTime) {
-            const finalTimer: Timer = {
-                id: v4(),
-                name: !!name ? name : 'Untitled',
-                time: storedMaxTime
-            };
-
-            ipcRenderer.send('save-recent', finalTimer);
-        }
-    }, []);
+    const { name } = useTimer();
 
     return (
         <>
             <Title>{name}</Title>
-            <Clock maxTime={storedMaxTime} time={storedTime} />
+            <Clock />
         </>
     );
 }
