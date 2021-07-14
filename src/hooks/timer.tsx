95ce26f02013a931ec1cfd011f704fdf9a3f7804
Tales Garcia <garcia.tales@gmail.com>
2021-07-14 19:10:25 -0300

    feat(hooks): create timer hook

---
+++ src/hooks/timer.tsx
@@ -0,0 +1,168 @@
+import { ipcRenderer, remote } from 'electron';
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
+import { v4 } from 'uuid';
+
+interface TimerContextContent {
+    time: number | null;
+    name: string;
+    isActive: boolean;
+    maxTime: number | null;
+    inputTime: {
+        seconds: string;
+        minutes: string;
+        hours: string;
+    };
+    setInputTime: React.Dispatch<React.SetStateAction<{
+        seconds: string;
+        minutes: string;
+        hours: string;
+    }>>;
+    toggleCounter: () => void;
+    resetCounter: () => void;
+}
+
+const TimerContext = createContext<TimerContextContent>({} as TimerContextContent);
+
+export const useTimer = () => {
+    const context = useContext(TimerContext);
+
+    if (!context) {
+        throw new Error('useTimer must be used within TimerProvider');
+    }
+
+    return context;
+}
+
+const {
+    storedTime, storedMaxTime, name, id
+} = ipcRenderer.sendSync('get-timer-props', remote.getCurrentWindow().id);
+
+export const TimerProvider: React.FC = ({ children }) => {
+    const [time, setTime] = useState<number | null>(storedTime || null);
+    const [isActive, setIsActive] = useState(false);
+    const [maxTime, setMaxTime] = useState<number | null>(storedMaxTime || storedTime || null);
+
+    const [inputTime, setInputTime] = useState({
+        seconds: '--',
+        minutes: '--',
+        hours: '00'
+    });
+
+    useEffect(() => {
+        remote.getCurrentWindow().setTitle(`${!!name ? `${name} - ` : 'Untitled - '}Timer`);
+
+        if (!!id) {
+            ipcRenderer.send('save-recent', id);
+        } else if (!!storedMaxTime) {
+            const finalTimer: Timer = {
+                id: v4(),
+                name: !!name ? name : 'Untitled',
+                time: storedMaxTime
+            };
+
+            ipcRenderer.send('save-recent', finalTimer);
+        }
+    }, []);
+
+    useEffect(() => {
+        ipcRenderer.on('set-time', (_, time) => {
+            if (!time) {
+                setInputTime({
+                    seconds: '--',
+                    minutes: '--',
+                    hours: '--'
+                });
+            } else {
+                setInputTime({
+                    hours: String(Math.floor(time / 3600)).padStart(2, '0'),
+                    minutes: String(Math.floor(time % 3600 / 60)).padStart(2, '0'),
+                    seconds: String(Math.floor(time % 3600 % 60)).padStart(2, '0')
+                })
+            }
+            setMaxTime(time || null);
+            setTime(time || null);
+            setIsActive(false);
+        });
+    }, []);
+
+    const toggleCounter = useCallback(
+        () => {
+            setIsActive(!isActive);
+        },
+        [isActive, setIsActive],
+    );
+
+    const resetCounter = useCallback(
+        () => {
+            setIsActive(false);
+            setTime(maxTime);
+            setInputTime({
+                seconds: '--',
+                minutes: '--',
+                hours: '--'
+            });
+        },
+        [maxTime]
+    );
+
+    const handleGetTimeReply = useCallback(() => {
+        ipcRenderer.send('get-time-reply', time, maxTime)
+    }, [time, maxTime])
+
+    useEffect(() => {
+        ipcRenderer.removeAllListeners('get-time');
+
+        if (time) {
+            ipcRenderer.addListener('get-time', handleGetTimeReply);
+        } else {
+            ipcRenderer.addListener('get-time', () => ipcRenderer.send('get-time-reply', null));
+        }
+
+        if (isActive && time && time > 0) {
+            const timeout = setTimeout(() => {
+                setTime(time - 1);
+                setInputTime({
+                    hours: String(Math.floor((time - 1) / 3600)).padStart(2, '0'),
+                    minutes: String(Math.floor((time - 1) % 3600 / 60)).padStart(2, '0'),
+                    seconds: String(Math.floor((time - 1) % 3600 % 60)).padStart(2, '0')
+                });
+            }, 1000);
+            return () => clearTimeout(timeout);
+        } else if (isActive && time === 0) {
+            resetCounter()
+            new Notification('Timer finished');
+        }
+    }, [isActive, time]);
+
+    useEffect(() => {
+        if (!Object.keys(inputTime).some(text => !(inputTime as any)[text] || isNaN(Number((inputTime as any)[text])))) {
+            const { minutes, seconds, hours } = inputTime;
+
+            const textTime = (Number(hours) * 3600) + (Number(minutes) * 60) + Number(seconds);
+
+            if (textTime === time) {
+                return;
+            }
+
+            setTime(textTime);
+            setMaxTime(textTime);
+        }
+    }, [inputTime]);
+
+    return (
+        <TimerContext.Provider
+            value={{
+                time,
+                name,
+                isActive,
+                maxTime,
+                inputTime,
+                setInputTime,
+                toggleCounter,
+                resetCounter
+            }}
+        >
+            {children}
+        </TimerContext.Provider>
+    )
+}
