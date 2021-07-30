8ce12216fe56ec4c61c725e15343c6f31b39e429
Tales Garcia <garcia.tales@gmail.com>
2021-07-30 14:35:56 -0300

    feat(routes): apply splash page to routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Settings from './pages/Settings';
+import Splash from './pages/Splash';
 import Timer from './pages/Timer';
 
 const Routes: React.FC = () => {
@@ -11,6 +12,7 @@ const Routes: React.FC = () => {
         <Route path="/timer" exact component={Timer} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/settings" component={Settings} />
+        <Route path="/splash" exact component={Splash} />
       </Switch>
     </HashRouter>
   );
