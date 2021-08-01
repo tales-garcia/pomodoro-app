74a5205d591467b24e41866577b4a44f86575c6a
Tales Garcia <garcia.tales@gmail.com>
2021-07-31 23:10:20 -0300

    refactor(hooks): set theme in theme helper hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,25 +2,18 @@ import React from 'react';
 import 'react-circular-progressbar/dist/styles.css';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { ThemeProvider } from 'styled-components'
 import { ModalProvider } from '../hooks/modal';
 import { WorkspaceProvider } from '../hooks/workspace';
 import { GlobalStyle } from '../styles/GlobalStyle'
 import { LocalizationProvider } from './localization';
+import { ThemeHelperProvider } from './theme';
 import { TimerProvider } from './timer';
-
-const theme = {
-    blue: '#24243B',
-    darkBlue: '#151423',
-    red: '#D93C3C',
-    text: '#E4E4E4'
-}
 
 const AppProvider: React.FC = ({ children }) => {
     return (
         <LocalizationProvider>
             <DndProvider backend={HTML5Backend}>
-                <ThemeProvider theme={theme}>
+                <ThemeHelperProvider>
                     <GlobalStyle />
                     <ModalProvider>
                         <WorkspaceProvider>
@@ -29,7 +22,7 @@ const AppProvider: React.FC = ({ children }) => {
                             </TimerProvider>
                         </WorkspaceProvider>
                     </ModalProvider>
-                </ThemeProvider>
+                </ThemeHelperProvider>
             </DndProvider>
         </LocalizationProvider>
     );
---
+++ src/hooks/theme.tsx
@@ -0,0 +1,41 @@
+import { ipcRenderer } from 'electron';
+import React, { createContext, useCallback, useContext, useEffect, useMemo } from 'react';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+interface ThemeHelperContextContent {
+}
+
+const themes = {
+    dark: {
+        blue: '#24243B',
+        darkBlue: '#151423',
+        red: '#D93C3C',
+        text: '#E4E4E4'
+    }
+}
+
+const ThemeHelperContext = createContext<ThemeHelperContextContent>({} as ThemeHelperContextContent);
+
+export const useThemeHelper = () => {
+    const context = useContext(ThemeHelperContext);
+
+    if (!context) {
+        throw new Error('useThemeHelper must be used within ThemeHelperProvider');
+    }
+
+    return context;
+}
+
+export const ThemeHelperProvider: React.FC = ({ children }) => {
+    return (
+        <ThemeHelperContext.Provider
+            value={{
+            }}
+        >
+            <ThemeProvider theme={themes.dark}>
+                {children}
+            </ThemeProvider>
+        </ThemeHelperContext.Provider>
+    )
+}
