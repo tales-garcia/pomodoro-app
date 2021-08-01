786ca16ecf3ac3b7fc80038309ec16f51d64f25e
Tales Garcia <garcia.tales@gmail.com>
2021-08-01 12:59:36 -0300

    feat(key styles): use theme color

--- src/components/Key/styles.ts
+++ src/components/Key/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.span`
     border-radius: 4px;
     padding: 4px 8px;
-    background-color: #1f1f33;
+    background-color: ${({ theme }) => theme.keyBackground};
     font: 500 0.8rem Montserrat, sans-serif !important;
     border: 1px solid ${({ theme }) => theme.darkBlue};
     border-bottom-width: 3px;
