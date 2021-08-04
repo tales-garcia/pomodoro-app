b3c8faf99a01e35391dc7e8d39155634550654a8
Tales Garcia <garcia.tales@gmail.com>
2021-08-04 17:46:46 -0300

    feat(settings): create first section of settings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Button from '../../components/Button';
+import Switch from '../../components/Switch';
 
 import { Container } from './styles';
 
 const Settings: React.FC = () => {
     return (
         <Container>
-            Settings
+            <h1>Settings</h1>
+
+            <section>
+                <h3>Recents</h3>
+                <ul>
+                    <li>
+                        <p>Clear Recents</p>
+                        <Button confirmButton>Execute</Button>
+                    </li>
+                    <li>
+                        <p>Enable Recents</p>
+                        <Switch name="enableRecents" />
+                    </li>
+                </ul>
+            </section>
         </Container>
     );
 }
--- src/pages/Settings/styles.ts
+++ src/pages/Settings/styles.ts
@@ -1,5 +1,36 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    padding: 0.4rem 2.2rem;
 
+    > h1 {
+        margin-bottom: 1.6rem;
+    }
+
+    > section {
+        > h3 {
+            font-size: 1.4rem;
+            margin-bottom: 0.6rem;
+        }
+
+        > ul {
+            background: ${({ theme }) => theme.darkBlue};
+            list-style: none;
+            border-radius: 8px;
+            padding: 1rem 1rem;
+
+            > li {
+                display: flex;
+                width: 100%;
+                align-items: center;
+                justify-content: space-between;
+
+                & + li {
+                    margin-top: 0.8rem;
+                }
+            }
+        }
+    }
 `;
