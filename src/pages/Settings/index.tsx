b0c99de603a3f73df99c8ce68d135f5e0a710571
Tales Garcia <garcia.tales@gmail.com>
2021-08-04 17:57:30 -0300

    feat(settings): finish settings page structure

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,5 +1,7 @@
+import { Form, Formik } from 'formik';
 import React from 'react';
 import Button from '../../components/Button';
+import Dropdown from '../../components/Dropdown';
 import Switch from '../../components/Switch';
 
 import { Container } from './styles';
@@ -9,19 +11,58 @@ const Settings: React.FC = () => {
         <Container>
             <h1>Settings</h1>
 
-            <section>
-                <h3>Recents</h3>
-                <ul>
-                    <li>
-                        <p>Clear Recents</p>
-                        <Button confirmButton>Execute</Button>
-                    </li>
-                    <li>
-                        <p>Enable Recents</p>
-                        <Switch name="enableRecents" />
-                    </li>
-                </ul>
-            </section>
+            <Formik
+                initialValues={{
+                    enableRecents: '',
+                    openLastSession: '',
+                    displaySplash: '',
+                    setTheme: '',
+                    setLanguage: ''
+                }}
+                onSubmit={console.log}
+            >
+                <Form>
+                    <section>
+                        <h3>Recents</h3>
+                        <ul>
+                            <li>
+                                <p>Clear Recents</p>
+                                <Button confirmButton>Execute</Button>
+                            </li>
+                            <li>
+                                <p>Enable Recents</p>
+                                <Switch name="enableRecents" />
+                            </li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h3>Initialize</h3>
+                        <ul>
+                            <li>
+                                <p>Open Last Session Windows</p>
+                                <Switch name="openLastSession" />
+                            </li>
+                            <li>
+                                <p>Display Splash Screen</p>
+                                <Switch name="displaySplash" />
+                            </li>
+                        </ul>
+                    </section>
+                    <section>
+                        <h3>Initialize</h3>
+                        <ul>
+                            <li>
+                                <p>Set Theme</p>
+                                <Dropdown name="setTheme" placeholder="Select a Theme" options={[{ label: 'Dark', value: 'dark' }]} />
+                            </li>
+                            <li>
+                                <p>Set Language</p>
+                                <Dropdown name="setLanguage" placeholder="Select a Language" options={[{ label: 'English', value: 'en-US' }]} />
+                            </li>
+                        </ul>
+                    </section>
+                </Form>
+            </Formik>
         </Container>
     );
 }
