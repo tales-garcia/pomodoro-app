04d7a0ddb175850f285d04e9d20deb5bd8c794c4
Tales Garcia <garcia.tales@gmail.com>
2021-08-04 18:04:31 -0300

    feat(switch): use formik on switch component

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,24 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import { Field, useField } from 'formik';
+import React from 'react';
 
 import { Container } from './styles';
 
 interface SwitchProps {
-  onChange?(checked: boolean): void;
   name: string;
 }
 
-const Switch: React.FC<SwitchProps> = ({ onChange, name }) => {
-  const [value, setValue] = useState(false);
-
-  useEffect(() => {
-    if (!!onChange) {
-      onChange(value);
-    }
-  }, [value]);
+const Switch: React.FC<SwitchProps> = ({ name }) => {
+  const [{ value }] = useField(name);
 
   return (
     <Container checked={Number(value)}>
-      <input value={`${value}`} onChange={ev => setValue(ev.currentTarget.checked)} type="checkbox" name={name} id={name} />
+      <Field type="checkbox" name={name} id={name} />
       <label htmlFor={name}>
         <span />
       </label>
