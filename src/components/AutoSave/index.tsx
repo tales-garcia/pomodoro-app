a9e3681638904c15746b8921b407dd6475dfbfbd
Tales Garcia <garcia.tales@gmail.com>
2021-08-11 20:10:21 -0300

    feat(auto save): check if its firt frame

--- src/components/AutoSave/index.tsx
+++ src/components/AutoSave/index.tsx
@@ -1,10 +1,16 @@
 import { useFormikContext } from 'formik';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const AutoSave: React.FC = () => {
     const { values, submitForm } = useFormikContext();
+    const ref = useRef<boolean>(true);
 
     useEffect(() => {
+        if (ref.current) {
+            ref.current = false;
+            return;
+        }
+
         submitForm();
     }, [values]);
 
