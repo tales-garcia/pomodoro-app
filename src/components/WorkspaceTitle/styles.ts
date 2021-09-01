5ba48e64788d96b27d461977753a4c0b090a7b27
Tales Garcia <garcia.tales@gmail.com>
2021-08-31 22:04:37 -0300

    feat(components): create woekspace title component

---
+++ src/components/WorkspaceTitle/styles.ts
@@ -0,0 +1,9 @@
+import ReactTooltip from 'react-tooltip';
+import styled from 'styled-components';
+
+export const WorkspaceTooltip = styled(ReactTooltip)`
+    background: #000 !important;
+    border-radius: 8px !important;
+    width: 14rem;
+    word-wrap: break-word;
+`;
